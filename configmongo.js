40b2c3b06f53a130d096c6736db8e13dd77cb378
Eduardo Ritter <eduardo.moraes.ritter@gmail.com>
2019-10-12 12:42:37 -0300

    shank

--- configmongo.js
+++ configmongo.js
@@ -1,6 +1,6 @@
 let env = process.env.NODE_ENV || 'development';
 let mongoConfig = {
-    'development': 'mongodb+srv://wrteste:d2BS7vl6uGASfHkR@cluster0-mamro.mongodb.net/test?retryWrites=true&w=majority',
+    'development': process.env.CON,
     'homolog': 'mongodb://localhost:27017/hml',
     'production': 'mongodb://localhost:27017/prd'
 };
